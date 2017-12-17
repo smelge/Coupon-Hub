37daf33a0849143bcdf7df4135cd97e5e3e9a1cc
smelge <traildogfilms@gmail.com>
2017-12-17 13:55:10 +0000

    Update members_link.php

--- site/includes/members_link.php
+++ site/includes/members_link.php
@@ -1,9 +1,9 @@
 <?php
 	//Categories Database
-	$hostname = "localhost";
-	$username = "thecoupo_member";
-	$password = ",y{68:%_53en3)Y";
+	$hostname = "#";
+	$username = "#";
+	$password = "#";
 
 	$secure_db = mysqli_connect($hostname, $username, $password);
-	mysqli_select_db($secure_db, "thecoupo_userbase") or die ("Can't find Member Database");
+	mysqli_select_db($secure_db, "#") or die ("Can't find Member Database");
 ?>
