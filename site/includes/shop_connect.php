0622fe64c8a7db3437179c15c42b8123d33a336a
smelge <traildogfilms@gmail.com>
2017-12-17 13:56:01 +0000

    Update shop_connect.php

--- site/includes/shop_connect.php
+++ site/includes/shop_connect.php
@@ -1,9 +1,9 @@
 <?php
 	//Shop Database Connections
-	$hostname = "localhost";
-	$username = "thecoupo_shop";
-	$password = "~:%[\&z];1K){7N";
+	$hostname = "#";
+	$username = "#";
+	$password = "#";
 
 	$shop_db = mysqli_connect($hostname, $username, $password);
-	mysqli_select_db($shop_db, "thecoupo_coupons") or die ("Can't find Shop Database");
+	mysqli_select_db($shop_db, "#") or die ("Can't find Shop Database");
 ?>
