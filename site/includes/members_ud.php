efd7af47cb1565269c585dd172506407dabeb7ae
smelge <traildogfilms@gmail.com>
2017-12-17 13:55:31 +0000

    Update members_ud.php

--- site/includes/members_ud.php
+++ site/includes/members_ud.php
@@ -1,9 +1,9 @@
 <?php
 	//Categories Database
-	$hostname = "localhost";
-	$username = "thecoupo_memupd";
-	$password = "-Y1mw@Ds;!\Cj^H";
+	$hostname = "#";
+	$username = "#";
+	$password = "#";
 
 	$mem_update = mysqli_connect($hostname, $username, $password);
-	mysqli_select_db($mem_update, "thecoupo_userbase") or die ("Can't find Member Database");
+	mysqli_select_db($mem_update, "#") or die ("Can't find Member Database");
 ?>
