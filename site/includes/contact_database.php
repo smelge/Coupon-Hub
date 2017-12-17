5a70b930165fb665c1a4884fe34acc26f4dee45f
smelge <traildogfilms@gmail.com>
2017-12-17 13:54:43 +0000

    Update contact_database.php

--- site/includes/contact_database.php
+++ site/includes/contact_database.php
@@ -1,9 +1,9 @@
 <?php
 	//Categories Database
-	$hostname = "localhost";
-	$username = "thecoupo_contact";
-	$password = "&Dgik(960r0Gwr";
+	$hostname = "#";
+	$username = "#";
+	$password = "#";
 
 	$dbContact = mysqli_connect($hostname, $username, $password);
-	mysqli_select_db($dbContact, "thecoupo_userbase") or die ("Can't update Contact Database");
+	mysqli_select_db($dbContact, "#") or die ("Can't update Contact Database");
 ?>
