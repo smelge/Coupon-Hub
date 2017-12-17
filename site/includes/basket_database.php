188fa9a4778a3288563149081adb096044483961
smelge <traildogfilms@gmail.com>
2017-12-17 13:53:52 +0000

    Update basket_database.php

--- site/includes/basket_database.php
+++ site/includes/basket_database.php
@@ -1,9 +1,9 @@
 <?php
 	//Shopping Basket Database
-	$hostname = "localhost";
-	$username = "thecoupo_grabs";
-	$password = "fI%HA@/Gfh1G:DU";
+	$hostname = "#";
+	$username = "#";
+	$password = "#";
 
 	$db_basket = mysqli_connect($hostname, $username, $password);
-	mysqli_select_db($db_basket, "thecoupo_baskets") or die ("Can't find baskets");
+	mysqli_select_db($db_basket, "#") or die ("Can't find baskets");
 ?>
